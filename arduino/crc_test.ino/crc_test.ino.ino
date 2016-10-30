862006d288550af7cdba849554d82061e04c72d0
Ferdinand Köppen <ferdinand.koeppen@bitmovin.com>
2016-10-30 19:18:44 +0100

    Added arduino crc test

---
+++ arduino/crc_test.ino/crc_test.ino.ino
@@ -0,0 +1,16 @@
+
+
+void transmit_data(int sensor, int value)
+{
+  
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
