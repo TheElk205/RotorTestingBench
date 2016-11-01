5dd871d41ff811bd1a9dff03e83bd3e7572bd1e0
Ferdinand Köppen <ferdinand.koeppen@bitmovin.com>
2016-11-01 18:57:16 +0100

    Added Demo plot with QT (sliding plot)

---
+++ python/guiQt/plotTestQT.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Various methods of drawing scrolling plots.
+"""
+
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui
+import numpy as np
+
+win = pg.GraphicsWindow()
+win.setWindowTitle('pyqtgraph example: Scrolling Plots')
+
+
+p2 = win.addPlot()
+data1 = np.random.normal(size=300)
+curve2 = p2.plot(data1)
+ptr1 = 0
+
+
+def update1():
+    global data1, ptr1
+    data1[:-1] = data1[1:]  # shift data in the array one sample left
+    # (see also: np.roll)
+    data1[-1] = np.random.normal()
+
+    ptr1 += 1
+    curve2.setData(data1)
+    curve2.setPos(ptr1, 0)
+
+
+# update all plots
+def update():
+    update1()
+
+
+timer = pg.QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(50)
+
+## Start Qt event loop unless running in interactive mode or using pyside.
+if __name__ == '__main__':
+    import sys
+
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
