2923080c2992cd806f4a1f482f1d59691d757a3e
Ferdinand Köppen <ferdinand.koeppen@bitmovin.com>
2016-11-10 15:18:05 +0100

    USed simple byte transmission. Still not perfect, as the timeframe is exceeded every time since the duration for the operations is not considered. Howerver, this delays the application significatnly don't know why

--- arduino/MainProgram.ino/MainProgram.ino.ino
+++ arduino/MainProgram.ino/MainProgram.ino.ino
@@ -2,14 +2,19 @@
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-unsigned long intervallMicros = 50000;
+unsigned long intervallMicros = 500e3;
 unsigned long lastIteration = 0;
+unsigned long iterationStarted = 0;
+unsigned long iterationDuration = 0;
 unsigned long timeDifference = 0;
+int iterationsDone = 0;
 
 int analogValues[] = {0,0,0,0,0};
 
 // Error Codes
 int errorIntervallTimeExceeded = 100;
+
+boolean inCycle = true;
 
 void setup()
 {
@@ -18,35 +23,34 @@ void setup()
   Serial.println("Started");
 
   lcd.begin(16, 2);
-  lcd.print("Rotor Testing Bench");
+  lcd.clear();
+  lcd.print("Rotor Testing");
   lastIteration = micros();
 }
 
 void loop()
 {
-  timeDifference = micros() - lastIteration;
-  if(timeDifference < intervallMicros)
-    delayMicroseconds(intervallMicros-timeDifference);
+  if(inCycle)
+  {
+    // Read all values here
+    readAnalogValues();
+  
+    // Send all values here
+    sendAnalogValues();
     
-  // Send error if intervall couldnt be reached
-  if(timeDifference > intervallMicros)
-    sendError(errorIntervallTimeExceeded);
-    
-  // Read all values here
-  readAnalogValues();
+    // Reset current time
+    inCycle = false;
+    lastIteration = micros();
+  }
+  else
+  {
+    timeDifference = micros() - lastIteration;
+    if(timeDifference >= intervallMicros)
+    {
+      inCycle = true;
+    }
+  }
 
-  // Send all values here
-  sendAnalogValues();
-
-  //Write to display
-  lcd.setCursor(0, 1);
-  lcd.print(timeDifference);
-  lcd.print(" of ");
-  lcd.print(intervallMicros);
-  lcd.print(" micros");
-    
-  // Reset current time
-  lastIteration = micros();  
 }
 
 void readAnalogValues()
@@ -72,6 +76,7 @@ void sendError(int errorCode)
 
 void sendData(int pin, int value, unsigned long timestamp)
 {
+  /*
   Serial.print("{\"pin\": ");
   Serial.print(pin);
   Serial.print(", \"value\": ");
@@ -79,5 +84,34 @@ void sendData(int pin, int value, unsigned long timestamp)
   Serial.print(", \"time\": ");
   Serial.print(timestamp);
   Serial.println("}");
+  */
+  //Start
+  Serial.write(0);
+  Serial.write(0);
+  Serial.write(0);
+  Serial.write(0);
+  Serial.write(0);
+
+  //Write pin
+  Serial.write(getNthByte(pin, 0));
+
+  //Write value
+  Serial.write(getNthByte(value, 0));
+
+  //Write Timestamp
+  Serial.write(getNthByte(timestamp, 0));
+  Serial.write(getNthByte(timestamp, 1));
+  Serial.write(getNthByte(timestamp, 2));
+  Serial.write(getNthByte(timestamp, 3));
+}
+
+char getNthByte(int number, int n)
+{
+  return (number >> (8*n)) & 0xff;
+}
+
+char getNthByte(unsigned long number, int n)
+{
+  return (number >> (8*n)) & 0xff;
 }
 
--- python/guiQt/SerialReader.py
+++ python/guiQt/SerialReader.py
@@ -8,6 +8,8 @@ import numpy as np
 import re
 import json
 
+from struct import unpack, calcsize
+
 class SerialReader (threading.Thread):
     values = [[] for y in range(5)]
     terminate = False
@@ -33,13 +35,34 @@ class SerialReader (threading.Thread):
         # Get lock to synchronize threads
         while not self.terminate:
             try:
-                self.read_from_serial()
+                self.read_bytes_from_serial()
                 self.counter += 1
             except serial.SerialException as serEx:
                 print("Serial exception: {0}".format(serEx))
                 self.terminate = True
+    def read_bytes_from_serial(self):
+        isMessage = False
+        if not isMessage:
+            print("Waiting for message: ")
+            zerosCount = 0
+            while zerosCount < 5:
+                bytes = self.arduino.read(1)
+                if unpack('B', bytes)[0] == 0:
+                    zerosCount = zerosCount +1
+                else:
+                    zerosCount = 0
+        print("Message Started: ")
+        pin = unpack('B', self.arduino.read(1))[0]
+        print("Pin: {0}".format(pin))
+        value = unpack('B', self.arduino.read(1))[0]
+        print("value: {0}".format(value))
+        time = unpack('I', self.arduino.read(4))[0]
+        print("time: {0}".format(time))
 
-    def read_from_serial(self):
+        valuepair = np.array([time, value])
+        self.values[pin].append(valuepair)
+
+    def read_json_from_serial(self):
         line = self.arduino.readline().decode("utf-8")
         print("line: " + line)
         if line.startswith("{"):
